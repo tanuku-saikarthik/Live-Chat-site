8838c75b74e67f40ffe365531ba2c47e5c98f227
tanuku-saikarthik <114074156+tanuku-saikarthik@users.noreply.github.com>
2024-06-10 03:25:29 +0530

    Update socket.js(have to change it back)

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,8 +9,8 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
+		origin: "*",
+		/*methods: ["GET", "POST"],*/
 	},
 });
 
